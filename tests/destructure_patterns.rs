72ab7e4624955bf6234552e07906cb865fddd30e
Idan Arye <IdanArye@gmail.com>
2018-10-31 10:05:57 +0200

    Add destructure patterns tests

--- tests/destructure_patterns.rs
+++ tests/destructure_patterns.rs
@@ -1,1 +1,74 @@
 extern crate inherent_pub;
+
+#[test]
+fn tuple_destructure() {
+    mod my_module {
+        use inherent_pub::inherent_pub;
+
+        pub trait Foo {
+            fn foo(self, a: (i32, i32)) -> i32;
+        }
+
+        pub struct Bar;
+
+        #[inherent_pub]
+        impl Foo for Bar {
+            pub fn foo(self, (a, b): (i32, i32)) -> i32 {
+                a + b
+            }
+        }
+    }
+
+    assert!(my_module::Bar.foo((40, 2)) == 42);
+}
+
+#[test]
+fn tuple_struct_destructure() {
+    mod my_module {
+        use inherent_pub::inherent_pub;
+
+        pub struct Pair(pub i32, pub i32);
+
+        pub trait Foo {
+            fn foo(self, a: Pair) -> i32;
+        }
+
+        pub struct Bar;
+
+        #[inherent_pub]
+        impl Foo for Bar {
+            pub fn foo(self, Pair(a, b): Pair) -> i32 {
+                a + b
+            }
+        }
+    }
+
+    assert!(my_module::Bar.foo(my_module::Pair(40, 2)) == 42);
+}
+
+#[test]
+fn struct_destructure() {
+    mod my_module {
+        use inherent_pub::inherent_pub;
+
+        pub struct Pair {
+            pub a: i32,
+            pub b: i32,
+        }
+
+        pub trait Foo {
+            fn foo(self, a: Pair) -> i32;
+        }
+
+        pub struct Bar;
+
+        #[inherent_pub]
+        impl Foo for Bar {
+            pub fn foo(self, Pair { a, b: c }: Pair) -> i32 {
+                a + c
+            }
+        }
+    }
+
+    assert!(my_module::Bar.foo(my_module::Pair{ a: 40, b: 2}) == 42);
+}
