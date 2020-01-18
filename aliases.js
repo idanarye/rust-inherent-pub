7e3a18feb762c490adb65232252ee4eb1a455369
Travis CI User <travis@example.org>
2020-01-18 17:30:04 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,6 @@
+var ALIASES = {};
+ALIASES["inherent_pub"] = {};
+ALIASES["proc_macro2"] = {};
+ALIASES["quote"] = {};
+ALIASES["syn"] = {};
+ALIASES["unicode_xid"] = {};
---
+++ inherent_pub/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"attr":[["inherent_pub","Make `pub` methods in the annotated `impl Trait for Type` block into inherited methods, which can be used without having to import the trait."]]});
